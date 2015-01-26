2056176b74ac63d7b60eb1df94b4c6cdcff443fd
roshan <roshan@jfbra.com>
2015-01-26 15:12:30 +0800

    add git completion shell

---
+++ install/git_completion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+sudo cp ./git-completion.bash /etc/bash_completion.d/
+echo 'if [ -f /etc/bash_completion.d/git-completion.bash ]; then
+    . /etc/bash_completion.d/git-completion.bash
+    fi' >> /etc/profile
